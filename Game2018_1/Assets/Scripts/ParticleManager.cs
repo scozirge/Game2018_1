1245880c0d91af9e9308ea5b13beaa24862d0536
scozirge <scozirge2@gmail.com>
2018-06-19 16:18:48 +0800

    fortest

---
+++ Game2018_1/Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+
+    float LifeTime;
+    private ParticleSystem ps;
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        LifeTime = ps.main.duration + ps.main.startLifetimeMultiplier + ps.main.startDelayMultiplier;
+    }
+    void Update()
+    {
+        LifeTime -= Time.deltaTime;
+        if (LifeTime <= 0)
+            Destroy(gameObject);
+    }
+}
